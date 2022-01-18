59db7cc84eae221443d039463d13298ca8613b4d
FftyShadesofCode <philiprmcdavid@gmail.com>
2022-01-17 21:38:16 -0500

    initial and final commit

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,27 +1,38 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+    // DO YOUR MAGIC
+    return db('accounts')
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+    // DO YOUR MAGIC
+    return db("accounts").where("id", id).first()
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+const create = async ({name, budget}) => {
+    // DO YOUR MAGIC
+    const [id] = await db('accounts').insert({name, budget})
+    return getById(id)
 }
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
+const updateById = async ({name, budget}) => {
+    // DO YOUR MAGIC
+    await db('accounts').where('id', id).update({name, budget})
+    return getById(id)
 }
 
-const deleteById = id => {
-  // DO YOUR MAGIC
+const deleteById = async (id) => {
+    // DO YOUR MAGIC
+    const deletedPost = await getById(id)
+    await db("accounts").where("id", id).delete()
+    return deletedPost
 }
 
 module.exports = {
-  getAll,
-  getById,
-  create,
-  updateById,
-  deleteById,
+    getAll,
+    getById,
+    create,
+    updateById,
+    deleteById,
 }
